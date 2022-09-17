10bf993e7bd4b922d9478623dbce886fab9a2240
Murilo gabriel nunes <murilognunes12@gmail.com>
2022-09-17 00:06:12 -0300

    Adicionar arquivos de projeto.

---
+++ Linq/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Resolucao
+    {
+        static void Main(string[] args)
+        {
+            int[] numero = new int[11];
+            
+            Console.WriteLine("Informe os números:");
+            for (int i = 0; i < 11; i++)
+            {
+                Console.Write("Numeros[" + i + "] :");
+                numero[i] = Convert.ToInt32(Console.ReadLine());
+            }
+             
+            int numimpar = (from n in numero select n).Where(n => n % 2 > 0).Count();
+
+            Console.WriteLine($"De todos os apresentados possui {numimpar} '+' impares presentes");
+            Console.ReadLine();
+        }
+    }
+}
